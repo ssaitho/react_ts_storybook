a355645c6e03d7681f3278da4c0684307d98461d
s-saito <s.s.saito108@gmail.com>
2022-04-27 19:25:46 +0900

    add file

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="pt-4 pl-4 text-red-600 text-4xl font-extrabold">
+          React+Tailwind CSS
+        </div>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
