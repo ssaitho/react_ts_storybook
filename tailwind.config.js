9074caee42afdb400188afa49e6c3676ac611b47
Shinsuke Saito <s.s.saito108@gmail.com>
2022-05-02 13:06:29 +0900

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", './public/index.html'
+    "./src/**/*.{js,jsx,ts,tsx}", './public/index.html', './dist/index.cjs.js'
   ],
   theme: {
     extend: {},
