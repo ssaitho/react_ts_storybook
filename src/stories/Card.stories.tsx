1faa51b87be9000afebd4876bebc3ab11279db04
Shinsuke Saito <s.s.saito108@gmail.com>
2022-05-20 19:19:04 +0900

    add test components

---
+++ src/stories/Card.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// コンポーネントのストーリーが、引数をpropsとして受け取る単純なコンポーネントである場合。
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Card } from './Card';
+
+export default {
+
+    // ブラウザで表示するタイトル
+    title: 'Example/Card',
+
+    // .storybook\main.jsの"stories"設定とマッチするファイルを探します。
+    // Buttonの場合はButton.tsxファイルを探しています。
+    component: Card,
+
+    // TypeScriptの型設定
+    // ComponentMetaは コンポーネントのストーリーが、引数をプロップとして受け取る単純なコンポーネントで使用します。
+} as ComponentMeta<typeof Card>;
+
+const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
+
+export const Card01 = Template.bind({});
