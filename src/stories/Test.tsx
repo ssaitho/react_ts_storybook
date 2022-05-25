43878fa29ae1cf91a6679762e3079671e03e57c4
Shinsuke Saito <s.s.saito108@gmail.com>
2022-05-25 12:26:28 +0900

    fix

--- src/stories/Test.tsx
+++ src/stories/Test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../index.css';
-import 'flowbite';
+// import 'flowbite';
 // import 'tw-elements';
 
 type User = {
@@ -18,7 +18,8 @@ export const Test = ({ user }: TestProps) => {
 
   return (
     <>
-      <button className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="defaultModal">
+      <p>コメントアウトしてます</p>
+      {/* <button className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="defaultModal">
         Toggle modal
       </button>
 
@@ -51,7 +52,7 @@ export const Test = ({ user }: TestProps) => {
             </div>
         </div>
     </div>
-</div>
+</div> */}
     </>
   )
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}', './public/index.html', './dist/index.cjs.js', './node_modules/flowbite/**/*.js'
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+    './dist/index.cjs.js',
+    // './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    // require('flowbite/plugin')
   ],
 }
