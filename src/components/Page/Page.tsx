b404f68abff3d4d924033997eb2ec7c2c744fb68
Shinsuke Saito <s.s.saito108@gmail.com>
2022-05-27 20:00:25 +0900

    fix

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -19,7 +19,7 @@ export const Page: React.VFC = () => {
         onCreateAccount={() => setUser({ name: 'Jane Doe' })}
       />
 
-      <section className='bg-cyan-500 shadow-lg shadow-cyan-500/50 rounded-lg'>
+      <section className='bg-ecforce-blue shadow-lg shadow-cyan-500/50 rounded-lg'>
         <h2>Pages in Storybook</h2>
         <p>
           We recommend building UIs with a{' '}
